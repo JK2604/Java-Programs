01d36763b722c2086493b46970c5d4660b0229d6
kuchi_0509 <derasarikartik@gmail.com>
2019-07-02 15:10:26 +0530

    commit

---
+++ Byteprog.java
@@ -0,0 +1,8 @@
+class byteprog
+{
+	public static void main(String[] args) {
+			byte b=5;
+			b=(byte)(b*5);
+			System.out.println(b);
+		}	
+}
---
+++ explicit.java
@@ -0,0 +1,9 @@
+class explicit
+{
+	public static void main(String[] args) {
+		byte b;
+		double d=342.342;
+		b=(byte)d;
+		System.out.println(b);
+	}
+}
